438266f5f03973f69b3aa4b2daa3f6fecf953f80
Dionisio <dionisio@naracosta.com.br>
2018-10-08 02:12:18 -0300

    Menu acrescentado

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express CRUD' });
+  var menu = 
+    [
+      {'title': 'Users', 'href': '/users'},
+      {'title': 'Products', 'href': '/products'}
+    ];
+  res.render('index', { title: 'Express CRUD', menu: menu });
 });
 
 module.exports = router;
