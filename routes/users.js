bdf8ed67c7327c8d83daf46f5d6a135138af1cc6
Dionisio <dionisio@naracosta.com.br>
2018-10-07 23:42:46 -0300

    Lista de usuários em ordem alfabética

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   req.getConnection(function(err,connection){     
-    var query = connection.query('SELECT * FROM user',function(err,rows) {
+    var query = connection.query('SELECT * FROM user ORDER BY name',function(err,rows) {
       if(err)
         console.log("Error Selecting : %s ",err );
       res.render('userlist', { users: rows } );
